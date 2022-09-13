8a9ff10883c6ee2ca8208737e7fb72aa0ca10915
Nathan Clevenger <nateclev@gmail.com>
2022-09-13 04:59:07 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,11 @@ export default {
     const { origin, pathname } = new URL(req.url)
     const module = await fetch('https://esb.deno.dev/https:/' + pathname).then(res => res.text())
     const id = req.headers.get('cf-ray')
-    
+    const metadata = JSON.stringify({
+      body_part: "script",
+      bindings: [],
+      tags: ['customer-123', 'staging', 'free-user']
+    })
     return new Response(JSON.stringify({ success: truel }, null, 2))
   }
 }
