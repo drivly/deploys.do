7a80b8ba51ba5e13006d9ac2af911ed4265a830d
Nathan Clevenger <nateclev@gmail.com>
2022-09-13 05:07:08 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,6 +8,21 @@ export default {
       bindings: [],
       tags: ['customer-123', 'staging', 'free-user']
     })
-    return new Response(JSON.stringify({ success: truel }, null, 2))
+    
+    const body = new FormData
+    body.append('metadata', '{ "main_module": "worker.js", "some_binding": "stuff" }')
+    body.append('"worker.js"', '@worker.js;type=application/javascript+module')
+    body.append('"second-file.js"', '@second-file.js;type=application/javascript+module')
+
+    const result = fetch(`https://api.cloudflare.com/client/v4/accounts/9a7806061c88ada191ed06f989cc3dac/workers/scripts/this-is_my_script-01`, {
+      body,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "Authorization": "Bearer " + env.CF_TOKEN,
+      },
+      method: "PUT"
+    })
+    
+    return new Response(JSON.stringify({ result }, null, 2))
   }
 }
